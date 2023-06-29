8d3594984a6e11aa1f95dbf3152b3fc94a8ec61e
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-06-29 14:45:47 -0500

    added hover over transform stranslate to anchor tags instead of icons fixed buttons

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -5,15 +5,15 @@ const ProjectItem = ({img, title, tech, link, g_link}) => {
     <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]' >
         <img className='rounded-xl group-hover:opacity-10' src={img} alt="" />
         <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]' >
-            <h3 className='text-2xl font-bold text-white tracking-wider text-center' >
+            <h3 className='text-lg font-bold text-white tracking-wider text-center' >
                 {title}
             </h3>
             <p className='pb-4 pt-2 text-white text-center' >{tech}</p>
             <a target="_blank" href={link}>
-                <p className='text-center p-3 text-white font-bold cursor-pointer text-lg  hover:scale-[150%] ease-in duration-200 hover:text-blue-800' >Click Here for Website</p>
+                <p className='bg-blue-800 rounded-3xl text-center p-2 m-2 text-white cursor-pointer text-xs  hover:scale-[120%] ease-in duration-200' >Click Here for Website</p>
             </a>
             <a target="_blank" href={g_link}>
-                <p className='text-center p-3 text-white font-bold cursor-pointer text-lg  hover:scale-[150%] ease-in duration-200 hover:text-blue-800' >Click Here for GitHub Repo</p>
+                <p className='bg-blue-800 rounded-3xl text-center p-2 m-2 text-white cursor-pointer text-xs  hover:scale-[120%] ease-in duration-200 ' >Click Here for GitHub Repo</p>
             </a>
         </div>
     </div>
