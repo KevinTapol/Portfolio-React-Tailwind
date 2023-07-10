b3bfab194153c5dbc076a902955ccb2396e1201c
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-07-10 11:05:31 -0500

    imported pics as src objects for images in main and blurb

--- src/components/Blurb.jsx
+++ src/components/Blurb.jsx
@@ -1,5 +1,6 @@
 import { useState, useRef, useEffect } from 'react'
 import React from 'react'
+import portrait from '../assets/imgs/profile.png'
 
 
 const Blurb = () => {
@@ -27,7 +28,8 @@ const Blurb = () => {
         </h1>
         {/* <span>{ myElementIsVisible ? 'Yes' : 'No' }</span> */}
         <div className='flex' >
-            <img className='hidden md:block rounded-full mr-3 shadow-2xl' style={{objectFit: 'cover', width: '100%', height: '100%'}} src="../src/assets/imgs/profile.png" alt="" />
+            {/* <img className='hidden md:block rounded-full mr-3 shadow-2xl' style={{objectFit: 'cover', width: '100%', height: '100%'}} src="../src/assets/imgs/profile.png" alt="" /> */}
+            <img className='hidden md:block rounded-full mr-3 shadow-2xl' style={{objectFit: 'cover', width: '100%', height: '100%'}} src={portrait} alt="" />
             <p className='m-auto' >During my 4 years of Software Engineering experience as a military veteran, I learned military software on the job while deployed overseas. I refactored several databases into a single system saving on average 2-4 hours of user experience for daily operations. This directly contributed to our team achieving the highest production rate to date of over 92%. I am looking forward to contributing by solving hard problems with an eye towards delivering customer value and business results through software engineering.</p>
         </div>
     
