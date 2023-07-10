9be8eb21ebd8346bf03879e4ff1eeae8af73d4fb
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-07-10 17:22:24 -0500

    added Meier's vacation pic

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,17 +3,18 @@ import { useInView } from 'react-intersection-observer'
 import { TypeAnimation } from 'react-type-animation'
 import Links from './Links'
 import matrix from '../assets/imgs/matrix.jpg'
+import meier from '../assets/imgs/meier.jpg'
 
 const Main = () => {
   const { ref:myRef, inView: myElementIsVisible } = useInView();
   return (
     <div ref={myRef} id='main' className='mb-1' >
         {/* <img className='w-full h-screen object-cover object-left scale-x' src="../src/assets/imgs/salute.jpg" alt="" /> */}
-        <img className='w-full h-screen object-fill object-left scale-x' src={matrix} alt="" />
+        <img className='w-full h-screen object-fill object-left scale-x' src={meier} alt="" />
         {/* to flip the image replace the class scale-x with scale-x-[-1] */}
         <div className='w-full h-screen absolute top-0 left-0 bg-white/40' >
             {/* <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center main_text' > */}
-            <div  className={`${myElementIsVisible ? 'max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center main_text' : 'max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center opacity-0' }`} >
+            <div  className={`${myElementIsVisible ? 'max-w-[700px] m-auto h-full w-full flex flex-col justify-center items-center main_text' : 'max-w-[700px] m-auto h-full w-full flex flex-col justify-center items-center opacity-0' }`} >
 
             
             <h1 className='sm:text-5xl text-4xl font-bold text-gray-800' >Kevin Tapolcsanyi</h1>
