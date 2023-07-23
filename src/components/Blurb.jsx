e6994286a97faa52fe29f7cf87f2d88a50a78922
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-07-23 08:07:49 -0500

    moved blurb id to child div

--- src/components/Blurb.jsx
+++ src/components/Blurb.jsx
@@ -18,11 +18,11 @@ const Blurb = () => {
   }, [])
 
   return (
-    <div ref={myRef} id='blurb' className={`${ myElementIsVisible ? 'max-w-[1040px] m-auto md:pl-20 p-4 py-16 fadeLoad' : 'max-w-[1040px] m-auto md:pl-20 p-4 py-16 opacity-0'} `}>
+    <div ref={myRef} className={`${ myElementIsVisible ? 'max-w-[1040px] m-auto md:pl-20 p-4 py-16 fadeLoad' : 'max-w-[1040px] m-auto md:pl-20 p-4 py-16 opacity-0'} `}>
         <h1 className='text-4xl font-bold text-center text-[#001b5e]' >
           Blurb
         </h1>
-        <div className='flex' >
+        <div id='blurb' className='flex' >
           <img className='hidden md:block rounded-full mr-3 shadow-2xl object-cover w-full h-full' src={portrait} alt="" />
           <p className='m-auto' >
             During my 4 years of Software Engineering experience as a military veteran, I learned military software on the job while deployed overseas. I refactored several databases into a single system saving on average 2-4 hours of user experience for daily operations. This directly contributed to our team achieving the highest production rate to date of over 92%. I am looking forward to contributing by solving hard problems with an eye towards delivering customer value and business results through software engineering.
