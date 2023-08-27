f2ff3e31f44db55af1d101ff73886c81fca81ff2
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-08-27 16:14:55 -0500

    updated resume to Alishah Novin's Template recommended by Taylor Dessyn

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -8,9 +8,12 @@ const Resume = () => {
         <h1 className='text-4xl mb-1 font-bold text-center text-[#001b5e]' >Resume
         </h1>
         <div className='flex' >
-            <a className='m-auto bg-blue-800 rounded-lg p-2 py-1 font-bold text-xl text-white cursor-pointer hover:scale-[110%] ease-in duration-200' target="_blank" href="https://docs.google.com/document/d/132_RyTjC0EIUiVMitXWQU0MopPzWk0olXqTD192C978/edit?usp=sharing">
+            <a className='m-auto bg-blue-800 rounded-lg p-2 py-1 font-bold text-xl text-white cursor-pointer hover:scale-[110%] ease-in duration-200' target="_blank" href="https://docs.google.com/document/d/1wP3IfR7SbS5jxYFLSOOX2hC9xnQ9VS0oFYr61QZs5OM/edit?usp=sharing">
               Click Here For My Resume
             </a>
+            {/* <a className='m-auto bg-blue-800 rounded-lg p-2 py-1 font-bold text-xl text-white cursor-pointer hover:scale-[110%] ease-in duration-200' target="_blank" href="https://docs.google.com/document/d/132_RyTjC0EIUiVMitXWQU0MopPzWk0olXqTD192C978/edit?usp=sharing">
+              Click Here For My Resume
+            </a> */}
         </div>
     </div>
   )
